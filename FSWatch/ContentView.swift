1560e493d7989be62b5aaa19e73a669cb53db6d5
Danny Hawkins <danny@quiqup.com>
2020-08-15 18:58:13 +0400

    Initial Commit

---
+++ FSWatch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FSWatch
+//
+//  Created by Danny Hawkins on 15/08/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
